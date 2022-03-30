8ceb64b263b2da6cc05379f128d3a6eafd6c6af7
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-30 12:05:15 +0700

    Add note to each image's source

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ require('dotenv').config()
 const key = process.env.key
 const endpoint = process.env.endpoint
 
-const url1 = 'https://en.wikipedia.org/wiki/Joker_(The_Dark_Knight)#/media/File:HeathJoker.png'
-const url2 = 'https://en.wikipedia.org/wiki/Heath_Ledger#/media/File:Heath_Ledger_(2).jpg'
+// Chris Evans
+const url1 = 'https://www.indiewire.com/wp-content/uploads/2019/05/chris-evans-1.jpg'
+
+// Captain America without masks
+const url2 = 'https://upload.wikimedia.org/wikipedia/commons/3/33/Mark_Kassen%2C_Tony_C%C3%A1rdenas_and_Chris_Evans_%28cropped%29.jpg'
+
+// Captain America behind the mask
+// const url2 = 'https://nypost.com/wp-content/uploads/sites/2/2021/01/chris-evans-captain-america.jpg'
 
 const main = async () => {
     const credentials = new msRest.ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } });
     const client = new Face.FaceClient(credentials, endpoint);
-
-    
 
     let faceDetectResult = await client.face.detectWithUrl(url1, {
         returnFaceId: true,
