ed5276d3aab52a71e5d4b9b6849ebf4e90b1a96b
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-30 12:06:48 +0700

    clear for live demo

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
-const Face = require('@azure/cognitiveservices-face')
-const msRest = require('@azure/ms-rest-js')
+
 require('dotenv').config()
 
 // don't forget to create a .env file with the following content:
@@ -17,29 +16,7 @@ const url2 = 'https://upload.wikimedia.org/wikipedia/commons/3/33/Mark_Kassen%2C
 // const url2 = 'https://nypost.com/wp-content/uploads/sites/2/2021/01/chris-evans-captain-america.jpg'
 
 const main = async () => {
-    const credentials = new msRest.ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } });
-    const client = new Face.FaceClient(credentials, endpoint);
-
-    let faceDetectResult = await client.face.detectWithUrl(url1, {
-        returnFaceId: true,
-        detectionModel: 'detection_03',
-        recognitionModel: 'recognition_04'
-    })
-    const faceId1 = faceDetectResult._response.parsedBody[0].faceId
-    console.log('Face Id 1:', faceId1)
-
-
-    faceDetectResult = await client.face.detectWithUrl(url2, {
-        returnFaceId: true,
-        detectionModel: 'detection_03',
-        recognitionModel: 'recognition_04'
-    })
-    const faceId2 = faceDetectResult._response.parsedBody[0].faceId
-    console.log('Face Id 2:', faceId2)
-
     
-    const result = await client.face.verifyFaceToFace(faceId1, faceId2)
-    console.log('Result:',result.isIdentical, result.confidence.toFixed(3))
 }
 
 main()
